9ebb1184bd76520641fa0bef6d4a85df95059e5f
dbillue <dbillue@gmail.com>
2019-10-10 09:58:28 -0400

    Added practice session 2 of 2

--- AV/SQL Scripts/PLSQL/D80182GC20_ag1 Chapter 8 Using Explicit Cursors.sql
+++ AV/SQL Scripts/PLSQL/D80182GC20_ag1 Chapter 8 Using Explicit Cursors.sql
@@ -7,21 +7,27 @@ Description:  D80182GC20_ag1 :: Chapter 8 Using Explicit Cursors
 SET SERVEROUTPUT ON
 SET AUTOPRINT ON
 
---desc employees
+--DESC employees
 
 DECLARE
-  v_deptno NUMBER := 80;
+  v_deptno NUMBER := 10;
   v_last_name employees.last_name%TYPE;
   v_salary employees.salary%TYPE;
   v_manager_id employees.manager_id%TYPE;
+  v_cursor_is_open BOOLEAN DEFAULT FALSE;
   
   CURSOR c_emp_cursor IS
   SELECT last_name, salary, manager_id FROM employees WHERE department_id IN (v_deptno) ORDER BY last_name;
+  
 BEGIN
-  OPEN c_emp_cursor;
+  IF NOT c_emp_cursor%ISOPEN THEN
+    OPEN c_emp_cursor;
+  END IF;
+  
   LOOP
     FETCH c_emp_cursor INTO v_last_name, v_salary, v_manager_id;
     EXIT WHEN c_emp_cursor%NOTFOUND;
+    
     IF v_salary < 5000 THEN
       CASE v_manager_id
         WHEN 101 THEN DBMS_OUTPUT.PUT_LINE(v_last_name || ' is due for a raise!');
@@ -35,4 +41,55 @@ BEGIN
 END;
 /
 
---SELECT * FROM employees ORDER BY last_name;
+--SELECT * FROM employees ORDER BY last_name
+
+--<><><><><><><><><><><><><><><><><*>--<~
+--<><><><><><><><><><><><><><><><><*>--<~
+
+-- DESC departments;
+-- DESC employees;
+
+DECLARE
+  v_dept_id departments.department_id%TYPE;
+  v_dept_name departments.department_name%TYPE;
+  v_emp_last_name employees.last_name%TYPE;
+  v_emp_job_id employees.job_id%TYPE;
+  v_emp_hire_date employees.hire_date%TYPE;
+  v_emp_salary employees.salary%TYPE;
+
+  CURSOR c_dept_cursor IS
+    SELECT department_id, department_name 
+    FROM departments 
+    WHERE department_id < 100 
+    ORDER BY department_id;
+  
+  CURSOR c_emp_cursor (v_dept_id NUMBER) IS
+    SELECT e.last_name, e.job_id, e.hire_date, e.salary 
+    FROM departments d JOIN employees e ON d.department_id = e.department_id 
+    WHERE e.employee_id < 120 AND d.department_id = v_dept_id;
+  
+BEGIN
+  IF NOT c_dept_cursor%ISOPEN THEN
+    OPEN c_dept_cursor;
+  END IF;
+  
+  LOOP <<department>>
+    FETCH c_dept_cursor INTO v_dept_id, v_dept_name;
+    EXIT WHEN c_dept_cursor%NOTFOUND;
+    DBMS_OUTPUT.PUT_LINE('Dept Id: ' || v_dept_id || ' Dept Name: ' || v_dept_name);
+  
+    LOOP
+      IF NOT c_emp_cursor%ISOPEN THEN
+        OPEN c_emp_cursor (v_dept_id);
+      END IF;
+      
+      FETCH c_emp_cursor INTO v_emp_last_name, v_emp_job_id, v_emp_hire_date, v_emp_salary;
+      EXIT WHEN c_emp_cursor%NOTFOUND;
+      DBMS_OUTPUT.PUT_LINE('Last Name: ' || v_emp_last_name || ' Job Id: ' || v_emp_job_id || ' Hire Date: ' || v_emp_hire_date || ' Emp Salary: ' || v_emp_salary);
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('-----------------------------------------');
+    CLOSE c_emp_cursor;
+  END LOOP;
+  CLOSE c_dept_cursor;
+END;
+/
