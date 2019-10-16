0358e5b9123a8c256a3422ad2423d7f1e41faa94
dbillue <dbillue@gmail.com>
2019-10-16 16:11:51 -0400

    Initial commit

---
+++ AV/SQL Scripts/PLSQL/Character_Functions.sql
@@ -0,0 +1,89 @@
+/*****************************
+Author:       Duane Billue
+Date:         2019-10-16
+Description:  Character Functions
+*****************************/
+-- Environment Variables
+--SET SERVEROUTPUT ON
+--SET AUTOPRINT ON
+
+--CREATE TABLE orders AS SELECT * FROM oe.orders;
+--CREATE TABLE employees AS SELECT * FROM hr.employees;
+
+-- CHR --
+SELECT CHR(68) || CHR(117) || CHR(97) || CHR(110) || CHR(101) "Name" FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- CONCAT --
+SELECT CONCAT(first_name, ' ' || last_name) "Full Name" FROM employees ORDER BY last_name;
+
+-- <><><><><><><><><><*>--<
+
+-- INITCAP --
+DECLARE
+    v_city_name VARCHAR2(250);
+    v_city_name_2 VARCHAR2(250);
+BEGIN
+    v_city_name := 'atlanta';
+    SELECT INITCAP(v_city_name) INTO v_city_name_2 FROM dual;
+    DBMS_OUTPUT.PUT_LINE(v_city_name_2);
+END;
+/
+ 
+-- <><><><><><><><><><*>--<
+
+-- LOWER --
+SELECT LOWER(last_name) FROM employees;
+
+-- <><><><><><><><><><*>--<
+
+-- UPPER --
+SELECT UPPER(first_name) FROM employees;
+
+-- <><><><><><><><><><*>--<
+
+-- LPAD --
+SELECT LPAD('<*>--<', 30, '<><>') FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- LTRIM --
+SELECT LTRIM('    Spaces Removed', ' ') FROM dual;
+SELECT LTRIM('<><><><><><><*>--<', '<>') FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- REPLACE --
+SELECT REPLACE('ABC123', '123', 'DEF') FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- RPAD --
+SELECT RPAD('<><><><><><><><><><><><>', 30, '<*>--<') FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- RTRIM --
+SELECT RTRIM('Spaces Removed     ', ' ') FROM dual;
+SELECT RTRIM('~--<*><><><><><><><>', '<->') FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- SUBSTR --
+SELECT SUBSTR('>--<*><><><><><><><>', 1, 12) FROM dual; 
+SELECT SUBSTR('<><><><><><><><*>--<', -12, 12) FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- TRANSLATE --
+SELECT TRANSLATE('1058 Gilbert St', ' ', '-') FROM dual;
+
+-- <><><><><><><><><><*>--<
+
+-- TRIM --
+SELECT TRIM(LEADING '*' FROM '**ORACLE**') FROM dual;
+SELECT TRIM(TRAILING '*' FROM '**ORACLE**') FROM dual;
+SELECT TRIM(BOTH '*' FROM '**ORACLE**') FROM dual;
+
+SELECT * FROM orders;
