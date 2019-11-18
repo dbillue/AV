2eb19462f18fe56bc62e8b69787733433a72b38f
dbillue <dbillue@gmail.com>
2019-11-18 07:48:45 -0500

    Updated comments

--- AV/Perl/fopen.pl
+++ AV/Perl/fopen.pl
@@ -1,3 +1,9 @@
+##############################
+# Author:		Duane Billue
+# Date:			2019-11-17
+# Description:	File open and write to file
+##############################
+
 $opened = open(FH, '>', 'C:\Data\Resume_files\blank.txt');
 print ($opened);
 $str = "yup";
--- AV/Perl/fread.pl
+++ AV/Perl/fread.pl
@@ -1,3 +1,9 @@
+##############################
+# Author:		Duane Billue
+# Date:			2019-11-17
+# Description:	File open and read / print to output
+##############################
+
 $opened = open(FH, '<', 'C:\Data\Resume_files\trace.html');
 $line = <FH>;
 #print ("First Line: "  $line);
