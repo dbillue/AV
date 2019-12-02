4906f5464cb84876b0bb8f3ffecc14f36e1410a3
dbillue <dbillue@gmail.com>
2019-12-02 17:18:54 -0500

    Inital Check In

---
+++ AV/CSharp/OraConn/OraConn/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+
+namespace OraConn
+{
+    class Program
+    {
+        static string oConnection = ConfigurationManager.AppSettings.Get("oraconn");
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                Console.WriteLine(oConnection);
+                Console.ReadLine();
+                using OracleConnection oConn = new OracleConnection(oConnection);
+                oConn.Open();
+                Console.WriteLine("Connection status {0}.", oConn.State);
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
