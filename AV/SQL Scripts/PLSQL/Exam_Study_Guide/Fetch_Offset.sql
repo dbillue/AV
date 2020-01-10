c7dad485c78b5cb9692cc4c35c026c27ddb7cfb0
Duane Billue <dbillue@gmail.com>
2020-01-10 09:02:52 -0500

    Update Fetch_Offset.sql
    Made minor code mods

--- AV/SQL Scripts/PLSQL/Exam_Study_Guide/Fetch_Offset.sql
+++ AV/SQL Scripts/PLSQL/Exam_Study_Guide/Fetch_Offset.sql
@@ -4,25 +4,28 @@ Date:         2020-01-07
 Description:  Exam Study Guide FETCH, OFFSET and WITH TIES statement
 *******************/
 
+SELECT * FROM aircraft_fleet_v ORDER BY apt_name, act_name;
+DESC aircraft_fleet_v;
+
 -- Use Rownum but unreliable.
 SELECT apt_name, apt_abbr, act_name
 FROM aircraft_fleet_v
 WHERE ROWNUM < 6
 ORDER BY apt_name, act_name;
 
--- Fetch clause.
+-- FETCH clause.
 SELECT apt_name, apt_abbr, act_name
 FROM aircraft_fleet_v
 ORDER BY apt_name, act_name
 FETCH FIRST 5 ROWS ONLY;
 
--- Offset clause.
+-- OFFSET clause.
 SELECT apt_name, apt_abbr, act_name
 FROM aircraft_fleet_v
 ORDER BY apt_name, act_name
 OFFSET 5 ROWS FETCH NEXT 5 ROWS ONLY;
 
--- FETCH / WITH TIES
+-- FETCH / WITH TIES :: Returns records with same values in ORDER BY clause.
 SELECT apt_name, apt_abbr, act_name
 FROM aircraft_fleet_v
 ORDER BY apt_name, act_name
@@ -32,5 +35,4 @@ FETCH FIRST 5 ROWS WITH TIES;
 SELECT apt_name, apt_abbr, act_name
 FROM aircraft_fleet_v
 ORDER BY apt_name, act_name
-FETCH FIRST 50 PERCENT ROWS ONLY;
-
+FETCH FIRST 75 PERCENT ROWS ONLY;
