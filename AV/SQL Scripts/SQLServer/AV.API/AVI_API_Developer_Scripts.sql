e240eaee1de9e2c29661ba6d4bd4b643e4261449
Duane Billue <dbillue@gmail.com>
2020-02-03 11:07:39 -0500

    Misc updates

--- AV/SQL Scripts/SQLServer/AV.API/AVI_API_Developer_Scripts.sql
+++ AV/SQL Scripts/SQLServer/AV.API/AVI_API_Developer_Scripts.sql
@@ -17,7 +17,24 @@ Persons
 ************/
 sp_help Persons
 
-SELECT * FROM Persons ORDER BY LastName
+SELECT * FROM Persons ORDER BY LastName, FirstName
+
+SELECT * FROM Persons
+WHERE 1 = 1
+	--AND PersonId IN ('8C11AFEA-F999-450E-A497-2F5545D038E7')
+	AND LastName <> 'Billue'
+
+SELECT person.*, bs.[State], pet.*
+FROM persons person LEFT OUTER JOIN pets pet ON person.PersonId = pet.PersonId
+	INNER JOIN birthstate bs ON bs.StateId = person.StateId
+ORDER BY person.FirstName, pet.Name
+
+INSERT INTO Persons (FirstName, MIddleName, LastName, Gender, Age, Country, City, StateId, DateOfBirth)
+VALUES ('Allison', 'Karly', 'Hope', 'Female', 34, 'USA', 'Unknown', 5, '1986-07-05')
+
+DELETE FROM Persons
+WHERE 1 = 1
+	AND PersonId IN ('04D883A7-ACCD-4A91-A02B-DB0B881C59EB', 'CEE7DC75-5B99-4DCE-B74E-5863781C8E26')
 
 
 /************
@@ -26,6 +43,10 @@ Pets
 sp_help Pets
 
 SELECT * FROM Pets ORDER BY Name
+
+SELECT pet.*, pettype.[Type]
+FROM pets pet INNER JOIN PetTypes pettype ON pet.PetTypeId = pettype.PetTypeId
+ORDER BY pettype.Type, pet.Name
 
 
 /************
@@ -37,8 +58,8 @@ SELECT * FROM PetTypes ORDER BY Type
 
 
 /************
-States
+BirthState
 ************/
-sp_help States
+sp_help BirthState
 
-SELECT * FROM States ORDER BY State
+SELECT * FROM BirthState ORDER BY State
