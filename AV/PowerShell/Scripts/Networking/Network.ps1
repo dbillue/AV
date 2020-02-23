319d59a05a548d3a893292531cdb9d273672ba3a
Duane Billue <dbillue@gmail.com>
2020-02-23 03:24:19 -0500

    Added additional networking classes.

--- AV/PowerShell/Scripts/Networking/Network.ps1
+++ AV/PowerShell/Scripts/Networking/Network.ps1
@@ -18,7 +18,54 @@ Get-NetRoute -AddressFamily IPv6
 '---------------------'
 Get-NetRoute -AddressFamily IPv4
 
+'---------------------'
+'Get Network adapters'
+'---------------------'
+Get-NetAdapter
 
+'---------------------'
+'MSFT_Net_Adapter'
+'---------------------'
+Get-WmiObject -ComputerName "." -Namespace Root\StandardCimv2 -class MSFT_NetAdapter
+
+'---------------------'
+'MSFT_Net_Adapter by device Id'
+'---------------------'
+$deviceID = "{B68B5DFD-90BA-4276-BD36-9C4674B47EA4}"
+Get-WmiObject -ComputerName "." -Namespace Root\StandardCimv2 -class MSFT_NetAdapter | Where-Object {$_.DeviceID -eq $deviceID} #> C:\Source\AV\PowerShell\Scripts\Networking\outout\Net_Adapter_WireLess.out
+
+'---------------------'
+'win32_networkadapter'
+'---------------------'
+Clear-Host
+get-wmiobject win32_networkadapter | Get-Member
+Get-WmiObject Win32_NetworkAdapter
+
+'---------------------'
+'win32_networkadapter by NetConnectionId'
+'---------------------'
+Clear-Host
+get-wmiobject win32_networkadapter | `
+    select DeviceID, netconnectionid, name, InterfaceIndex, Status, netconnectionstatus, description, netenabled, GUID | `
+    Where-Object {$_.netconnectionid -eq 'Wi-Fi'}
+
+'---------------------'
+'Win32_NetworkConnection !!! Fail'
+'---------------------'
+Clear-Host
+get-wmiobject -Class Win32_NetworkConnection
+
+'---------------------'
+'Win32_NetworkAdapterConfiguration'
+'---------------------'
+Clear-Host
+#Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Get-Member > C:\Source\AV\PowerShell\Scripts\Networking\outout\Win32_NetworkAdapterConfiguration_Members.out
+Get-WmiObject -Class Win32_NetworkAdapterConfiguration | `
+    select ServiceName, IPAddress, DefaultIPGateway, dhcpenabled, DNSDomain, DNSHostName, MACAddress, Description | `
+    Where-Object {$_.ServiceName -eq 'RTWlanE'}
+
+'###############################################'
+'###############################################'
 Clear-Host
 
 Get-WmiObject -Class Win32_Product -ComputerName . > C:\temp\CimInstance_Win32_BIOS.txt
@@ -31,6 +78,7 @@ Clear-Host
 Get-WmiObject Win32_Product | Sort-Object Name | `
 Format-Table Name, Version, Vendor
 
-Get-CimInstance -ClassName Win32_BIOS -ComputerName Cradle-Laptop > C:\temp\CimInstance_Win32_BIOS.txt
+Get-CimInstance -ClassName Win32_BIOS -ComputerName DESKTOP-2BFUPF5 > C:\temp\CimInstance_Win32_BIOS.txt
 
 Get-CimInstance -ClassName Win32_Desktop -ComputerName .
+
