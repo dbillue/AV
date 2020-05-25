e41296ceb60eaed7ba0cf81ac9912640625e940d
Duane Billue <dbillue@gmail.com>
2020-05-24 20:22:47 -0400

    Added folder stucture for better organization

---
+++ AV/AngularJS/Controllers/ReferenceScripts/simpleController.js
@@ -0,0 +1,10 @@
+/* 
+Author: 		Duane Billue
+Date:			2020-05-24
+Description:	AngularJS Reference - [Application / Module Include File]
+*/
+
+app.controller('simpleController', function($scope) {
+	$scope.firstName = "Jason";
+	$scope.lastName = "Greenfield";
+});
