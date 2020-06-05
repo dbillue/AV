4927abb3efc56a5be8561cbbc4b354085523ce4d
Duane Billue <dbillue@gmail.com>
2020-06-05 10:45:24 -0400

    Added control statements and comments

--- AV/TypeScript/CP_Intro.ts
+++ AV/TypeScript/CP_Intro.ts
@@ -1,3 +1,9 @@
+/******************
+ Author:        Duane Billue
+ Date:          2020-06-05
+ Description:   Basic TypeScript variables, object and statements.
+ ******************/
+
 // CP_Intro
 console.clear();
 var speedMPH:number = 25;
@@ -20,7 +26,42 @@ distance.DetermineDistance();
 // CP_Type_Assertion
 var str = 5;
 var str2:number = <number> <any> str;
-console.log(typeof(str2));
+console.log("Data type: " + typeof(str2));
+
+// CP_Conditional_Loops
+var cnt:number = 10;
+var totalCnt:number = 15;
+// While Loop
+while (cnt <= totalCnt) {
+    console.log("Current number in while loop: " + cnt);
+    cnt += 1;
+}
+
+cnt = 5;
+// Do While Loop
+do {
+    console.log("Current number in do while loop: " + cnt);
+    cnt += 1;
+ } while (cnt <= totalCnt);
+
+ // While Loop w/ Break statement
+ cnt = 1;
+ while (cnt <= totalCnt) {
+     if(cnt == (totalCnt / 3)) {
+        console.log("Break statement reached: " + cnt);
+         break;
+     }
+     cnt++;
+}
+
+// For Next Loop
+for(cnt = 0; cnt <= totalCnt; cnt++)
+{
+    if(cnt <= (totalCnt / 3)) {
+        console.log("Continue statement in effect: " + cnt);
+        continue;
+    }
+}
 
 
 // THIS IS FUN!!!!
