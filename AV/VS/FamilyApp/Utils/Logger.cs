1768df4c500e3db1794a9ae160f70b8059e3209d
Duane Billue <dbillue@gmail.com>
2020-06-27 12:03:50 -0400

    Adding logging

---
+++ AV/VS/FamilyApp/Utils/Logger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyApp.Utils
+{
+    public class Logger
+    {
+        private readonly ILogger _logger;
+
+        public Logger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteInformation(string message)
+        {
+            _logger.LogInformation("Message:" + message);
+        }
+
+        public void WriteError(string message, Exception ex)
+        {
+            _logger.LogInformation("Message:" + message);
+            _logger.LogInformation("Exception:" + ex.ToString());
+        }
+    }
+}
