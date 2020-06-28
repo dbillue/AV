1349d6254387319c1e900181e2166eae8deec550
Duane Billue <dbillue@gmail.com>
2020-06-28 09:52:49 -0400

    Added basic error handling

--- AV/VS/FamilyApp/Pages/FetchData.razor.cs
+++ AV/VS/FamilyApp/Pages/FetchData.razor.cs
@@ -17,11 +17,18 @@ namespace FamilyApp.Pages
         WeatherForecast weatherForecast = new WeatherForecast();
 
         bool ShowPopup;
+        bool error;
 
         protected override async Task OnInitializedAsync()
         {
-            var user = (await authenticationStateTask).User;
-            forecasts = await ForecastService.GetWeatherEntriesAsync(user.Identity.Name);
+            try
+            {
+                var user = (await authenticationStateTask).User;
+                forecasts = await ForecastService.GetWeatherEntriesAsync(user.Identity.Name);
+            } catch (Exception ex) {
+                error = true;
+                //TODO - Add error logging to file or data repo
+            }
         }
 
         void ClosePopup()
@@ -31,47 +38,71 @@ namespace FamilyApp.Pages
 
         void AddNewForeCast()
         {
-            weatherForecast = new WeatherForecast();
-            weatherForecast.Id = 0;
-            ShowPopup = true;
-            logger.LogInformation("Add new weather observation instantiated...");
+            try
+            {
+                weatherForecast = new WeatherForecast();
+                weatherForecast.Id = 0;
+                ShowPopup = true;
+                logger.LogInformation("Add new weather observation instantiated...");
+            } catch (Exception ex) {
+                error = true;
+                //TODO - Add error logging to file or data repo
+            }
         }
 
         async Task SaveObservation()
         {
-            ShowPopup = false;
-            var user = (await authenticationStateTask).User;
-            if (weatherForecast.Id == 0)
+            try
             {
-                WeatherForecast newWeatherObservation = new WeatherForecast();
-                newWeatherObservation.Date = System.DateTime.Now;
-                newWeatherObservation.Summary = weatherForecast.Summary;
-                newWeatherObservation.TemperatureC = Convert.ToInt32(weatherForecast.TemperatureC);
-                newWeatherObservation.UserName = user.Identity.Name;
-                var result = ForecastService.CreateWeatherObservationAsync(newWeatherObservation);
-                logger.LogInformation("New weather observation saved...");
-            }
-            else
-            {
-                logger.LogInformation("Weather observation saved...");
-            }
+                ShowPopup = false;
+                var user = (await authenticationStateTask).User;
+                if (weatherForecast.Id == 0)
+                {
+                    WeatherForecast newWeatherObservation = new WeatherForecast();
+                    newWeatherObservation.Date = System.DateTime.Now;
+                    newWeatherObservation.Summary = weatherForecast.Summary;
+                    newWeatherObservation.TemperatureC = Convert.ToInt32(weatherForecast.TemperatureC);
+                    newWeatherObservation.UserName = user.Identity.Name;
+                    var result = ForecastService.CreateWeatherObservationAsync(newWeatherObservation);
+                    logger.LogInformation("New weather observation saved...");
+                }
+                else
+                {
+                    logger.LogInformation("Weather observation saved...");
+                }
 
-            forecasts = await ForecastService.GetWeatherEntriesAsync(user.Identity.Name);
+                forecasts = await ForecastService.GetWeatherEntriesAsync(user.Identity.Name);
+            } catch (Exception ex) {
+                error = true;
+                //TODO - Add error logging to file or data repo
+            }
         }
 
         void EditForecast(WeatherForecast weatherObservation)
         {
-            weatherForecast = weatherObservation;
-            ShowPopup = true;
-            logger.LogInformation("Weather observation edited...");
+            try
+            {
+                weatherForecast = weatherObservation;
+                ShowPopup = true;
+                logger.LogInformation("Weather observation edited...");
+            } catch (Exception ex) {
+                error = true;
+                //TODO - Add error logging to file or data repo
+            }
         }
 
         async Task DeleteObservation()
         {
-            var user = (await authenticationStateTask).User;
-            var result = ForecastService.DeleteObservation(weatherForecast);
-            logger.LogInformation("Weather observation deleted...");
-            forecasts = await ForecastService.GetWeatherEntriesAsync(user.Identity.Name);
+            try
+            {
+                var user = (await authenticationStateTask).User;
+                var result = ForecastService.DeleteObservation(weatherForecast);
+                logger.LogInformation("Weather observation deleted...");
+                forecasts = await ForecastService.GetWeatherEntriesAsync(user.Identity.Name);
+            } catch (Exception ex) {
+                error = true;
+                //TODO - Add error logging to file or data repo
+            }
         }
     }
 }
