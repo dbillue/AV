3425d5362cb7231bbf85cae7073206384a9fc8e6
Duane Billue <dbillue@gmail.com>
2020-06-29 09:36:11 -0400

    Added Serilog logging configuration

--- AV/VS/FamilyApp/Program.cs
+++ AV/VS/FamilyApp/Program.cs
@@ -8,14 +8,43 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Json;
 
 namespace FamilyApp
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .WriteTo.File(new JsonFormatter(), Configuration["LogFileDirectory"] + "BlazorFamilyApp.Log")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application starting - " + DateTime.Now.ToString());
+                Log.Information("Machine Name - " + Environment.MachineName);
+                Log.Information("Environment Name - " + Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+                Log.Information("Environment User - " + Environment.UserName);
+                Log.Information("Environment Version - " + Environment.Version);
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -23,6 +52,7 @@ namespace FamilyApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
