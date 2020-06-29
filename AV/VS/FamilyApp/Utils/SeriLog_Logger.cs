61000528cc57ec499af58b51eb25a2b20acf055f
Duane Billue <dbillue@gmail.com>
2020-06-29 10:20:23 -0400

    Initial check in

---
+++ AV/VS/FamilyApp/Utils/SeriLog_Logger.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyApp.Utils
+{
+    public class SeriLog_Logger
+    {
+        public SeriLog_Logger() { }
+
+        public void WriteInformation(string msg)
+        {
+            try
+            {
+                Log.Information(msg);
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
