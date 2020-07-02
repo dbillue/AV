d66c796405d122ed4fc22f79cfd1ee6520fdd7d2
Duane Billue <dbillue@gmail.com>
2020-07-02 09:36:20 -0400

    Rechecked in

---
+++ AV/VS/BlazorClientSongList/Songs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClientSongList
+{
+    public class Songs
+    {
+        public string SongName { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
