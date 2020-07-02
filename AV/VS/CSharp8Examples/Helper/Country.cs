d930746a3c751cee842c8597367ede73c159d64e
Duane Billue <dbillue@gmail.com>
2020-07-02 07:58:30 -0400

    Removal and addition mistake

---
+++ AV/VS/CSharp8Examples/Helper/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8Examples.Helper
+{
+    public class Country
+    {
+        public string name;
+
+        public string hemisphere;
+    }
+}
