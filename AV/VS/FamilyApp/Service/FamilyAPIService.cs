7c09ebbef6952352ac74786fa20db7038dc76229
Duane Billue <dbillue@gmail.com>
2020-07-08 10:54:15 -0400

    Added REST API service class

---
+++ AV/VS/FamilyApp/Service/FamilyAPIService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using FamilyApp.Model;
+
+namespace FamilyApp.Service
+{
+    public class FamilyAPIService : IFamilyAPIService
+    {
+        public FamilyAPIService() { }
+
+        public async Task<string> GetPeopleRaw()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                //TODO: Move to startup and add URI to JSON config
+                httpClient.BaseAddress = new Uri("http://192.168.0.5:15001/");
+                return await httpClient.GetStringAsync("/api/persons");
+            }
+        }
+    }
+}
---
+++ AV/VS/FamilyApp/Service/IFamilyAPIService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FamilyApp.Model;
+
+namespace FamilyApp.Service
+{
+    interface IFamilyAPIService
+    {
+        Task<string> GetPeopleRaw();
+    }
+}
