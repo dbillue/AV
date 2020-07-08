914fa17c5a52a5f76c2fc6e959811f1e18ba26c3
Duane Billue <dbillue@gmail.com>
2020-07-08 17:16:03 -0400

    Added values to be sourced from configuration file

--- AV/VS/FamilyApp/Service/FamilyAPIService.cs
+++ AV/VS/FamilyApp/Service/FamilyAPIService.cs
@@ -1,24 +1,33 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using FamilyApp.Model;
+using Microsoft.Extensions.Configuration;
 
 namespace FamilyApp.Service
 {
     public class FamilyAPIService : IFamilyAPIService
     {
-        public FamilyAPIService() { }
+        IConfiguration configuration;
+        string URIEndPoint, URI_Path = string.Empty;
+
+        public FamilyAPIService(IConfiguration _configuration) 
+        {
+            configuration = _configuration;
+            URIEndPoint = configuration.GetSection("FamilyAPI").GetSection("URI").Value;
+            URI_Path = configuration.GetSection("FamilyAPI").GetSection("URI_Path").Value;
+        }
 
         public async Task<string> GetPeopleRaw()
         {
             using (var httpClient = new HttpClient())
             {
-                //TODO: Move to startup and add URI to JSON config
-                httpClient.BaseAddress = new Uri("http://192.168.0.5:15001/");
-                return await httpClient.GetStringAsync("/api/persons");
+                httpClient.BaseAddress = new Uri(URIEndPoint);
+                return await httpClient.GetStringAsync(URI_Path);
             }
         }
     }
