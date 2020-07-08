774e06598015e25182f0ff51de3a4a068790dde4
Duane Billue <dbillue@gmail.com>
2020-07-08 17:17:00 -0400

    Initial check in

---
+++ AV/VS/FamilyApp/Service/IPetService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FamilyApp.Model;
+
+namespace FamilyApp.Service
+{
+    interface IPetService
+    {
+        Task AddNewPet(Person person, Pet pet, List<Pet> petList, PetTypes petType, string personType);
+
+        int GetPetType(Pet pet, PetTypes petType);
+    }
+}
---
+++ AV/VS/FamilyApp/Service/PetService.cs
@@ -0,0 +1,69 @@
+﻿using FamilyApp.DBContext;
+using FamilyApp.Model;
+using FamilyApp.Pages;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FamilyApp.Service
+{
+    public class PetService : IPetService
+    {
+        private readonly FamilyService _familyService;
+
+        public PetService(IServiceProvider serviceProvider) 
+        {
+            _familyService = (FamilyService)serviceProvider.GetService<IFamilyService>();
+        }
+
+        public async Task AddNewPet(Person person, Pet pet, List<Pet> petList, PetTypes petType, string personType)
+        {
+            // Assign default properties.
+            pet.PersonId = person.PersonId;
+            pet.CreateDate = DateTime.Now;
+            pet.PetTypeId = GetPetType(pet, petType);
+
+            switch (personType)
+            {
+                case "New":
+                    await _familyService.AddPet(pet);
+                    break;
+                case "Existing":
+                    foreach (var p in petList)
+                    {
+                        if (string.IsNullOrEmpty(p.PersonId.ToString()))
+                        {
+                            await _familyService.AddPet(pet);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public int GetPetType(Pet pet, PetTypes petType)
+        {
+            int petTypeId = 0;
+
+            switch (petType.Type)
+            {
+                case "Cat":
+                    petTypeId = 1;
+                    break;
+                case "Dog":
+                    petTypeId = 2;
+                    break;
+                case "Reptile":
+                    petTypeId = 3;
+                    break;
+                default:
+                    break;
+            }
+
+            return petTypeId;
+        }
+    }
+}
