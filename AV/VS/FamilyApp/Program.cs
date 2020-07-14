db66d8e8527e37e7ecd35d1f8eda611c4ad3833b
Duane Billue <dbillue@gmail.com>
2020-07-14 08:58:49 -0400

    Removed non-needed using statements.

--- AV/VS/FamilyApp/Program.cs
+++ AV/VS/FamilyApp/Program.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Formatting.Json;
+using System;
+using System.IO;
 
 namespace FamilyApp
 {
--- AV/VS/FamilyApp/Startup.cs
+++ AV/VS/FamilyApp/Startup.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FamilyApp.Areas.Identity;
+using FamilyApp.DBContext;
+using FamilyApp.Service;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using FamilyApp.Areas.Identity;
-using FamilyApp.Service;
-using FamilyApp.DBContext;
 
 namespace FamilyApp
 {
