f1de35410f0a69f885cf862a994cb3028fe7d773
Duane Billue <dbillue@gmail.com>
2020-07-15 18:13:25 -0400

    Initial check in

---
+++ AV/VS/XUnitTestFamilyApp/DataObjects/DataObjs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FamilyApp.Model;
+
+namespace XUnitTestFamilyApp.DataObjects
+{
+    public class DataObjs
+    {
+        public Person GetPerson()
+        { 
+            Person person = new Person
+            {
+                PersonId = Guid.NewGuid()
+            };
+
+            return person;
+        }
+
+        public Pet GetPet()
+        { 
+            Pet pet = new Pet
+            {
+                Name = "Hay",
+                NickName = "HayHay",
+                PetTypeId = (int)PetType.Cat,
+                PersonId = Guid.Parse("e2216454-153a-4a3a-9d13-9c2a4abe1e0d"),
+                CreateDate = DateTime.Now,
+            };
+
+            return pet;
+        }
+
+        public PetTypes GetPetTypes(string petTypeName)
+        {
+            int petTypeId = 0;
+            string type = string.Empty;
+
+            switch (petTypeName)
+            {
+                case "Cat":
+                    petTypeId = 1;
+                    break;
+                case "Dog":
+                    petTypeId = 2;
+                    break;
+                case "Reptile":
+                    petTypeId = 3;
+                    break;
+                default:
+                    break;
+            }
+
+            PetTypes petType = new PetTypes
+            {
+                PetTypeId = petTypeId,
+                Type = petTypeName
+            };
+
+            return petType;
+        }
+    }
+}
---
+++ AV/VS/XUnitTestFamilyApp/PetTest.cs
@@ -0,0 +1,60 @@
+using FamilyApp.Service;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using XUnitTestFamilyApp.DataObjects;
+
+namespace XUnitTestFamilyApp
+{
+    public class PetTest
+    {
+        private readonly DataObjs dataObjs = new DataObjs();
+
+        [Fact]
+        public async void TestAddPet()
+        {
+            var pet = dataObjs.GetPet();
+            var person = dataObjs.GetPerson();
+            var petType = dataObjs.GetPetTypes(PetType.Cat.ToString());
+
+            // Arrange.
+            var petService = new Mock<IPetService>();
+            petService.Setup(ps => ps.AddNewPet(person, pet, petType)).ReturnsAsync(true);
+            var addPet = petService.Object;
+
+            // Act.
+            var actual = await addPet.AddNewPet(person, pet, petType);
+
+            // Assert.
+            Assert.True
+                (actual);
+        }
+
+        [Fact]
+        public void TestGetPetType()
+        {
+            var pet = dataObjs.GetPet();
+            var person = dataObjs.GetPerson();
+            var petType = dataObjs.GetPetTypes(PetType.Cat.ToString());
+
+            // Arrange.
+            var petService = new Mock<IPetService>();
+            petService.Setup(ps => ps.GetPetType(pet, petType)).Returns(1);
+            var ps = petService.Object;
+
+            // Act.
+            var actual = ps.GetPetType(pet, petType);
+
+            // Assert.
+            Assert.Equal
+                (1, actual);
+        }
+    }
+
+    enum PetType
+    {
+        Cat = 1,
+        Dog = 2,
+        Reptile = 3
+    }
+}
