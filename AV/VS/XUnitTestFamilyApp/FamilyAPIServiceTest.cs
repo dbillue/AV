2e84445c0606289643408ee7c5bf6d96fc137ed8
Duane Billue <dbillue@gmail.com>
2020-07-16 09:30:17 -0400

    Initial check in.

---
+++ AV/VS/XUnitTestFamilyApp/FamilyAPIServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using FamilyApp.Service;
+using Moq;
+using Xunit;
+
+namespace XUnitTestFamilyApp
+{
+    public class FamilyAPIServiceTest
+    {
+        [Fact]
+        public async void TestGetPeopleRaw()
+        {
+            // Arrange.
+            var familyAPIService = new Mock<IFamilyAPIService>();
+            familyAPIService.Setup(fsAPI => fsAPI.GetPeopleRaw()).ReturnsAsync("");
+            var famAPIService = familyAPIService.Object;
+
+            // Act.
+            var result = await famAPIService.GetPeopleRaw();
+
+            // Assert.
+            Assert.True
+                (result == string.Empty);
+        }
+    }
+}
