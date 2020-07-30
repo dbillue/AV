e7d189e155c1b9dbfba0c4ffc60a254be5d9870d
Duane Billue <dbillue@gmail.com>
2020-07-30 11:48:31 -0400

    Initial check in.

---
+++ AV/VS/FamilyApp/DTO/Pets.cs
@@ -0,0 +1,23 @@
+﻿using FamilyApp.Model;
+using System.Collections.Generic;
+
+namespace FamilyApp.DTO
+{
+    public class PetDTO
+    {
+        public string Name { get; set; }
+
+        public string NickName { get; set; }
+
+        public int PetTypeId { get; set; }
+
+        public string petType = string.Empty;
+        public List<PetTypes> petTypes;
+
+        public PetDTO()
+        {
+            petType = string.Empty;
+            petTypes = new List<PetTypes>();
+        }
+    }
+}
