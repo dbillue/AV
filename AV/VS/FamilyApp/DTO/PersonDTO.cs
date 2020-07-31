f5f666fc20278eb37df05a7b01fce0b23dd0fa8f
Duane Billue <dbillue@gmail.com>
2020-07-31 17:10:43 -0400

    Initial check in.

---
+++ AV/VS/FamilyApp/DTO/PersonDTO.cs
@@ -0,0 +1,42 @@
+﻿using FamilyApp.Model;
+using System;
+using System.Collections.Generic;
+
+namespace FamilyApp.DTO
+{
+    public class PersonDTO
+    {
+        public Guid PersonId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MIddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public int StateId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public List<Pet> Pets;
+        public List<BirthState> birthState;
+        public string state;
+
+        public PersonDTO()
+        {
+            Pets = new List<Pet>();
+            birthState = new List<BirthState>();
+            state = string.Empty;
+        }
+    }
+}
