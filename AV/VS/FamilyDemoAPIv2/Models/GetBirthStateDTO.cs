48854fd30f62e211db0eda2420a6408dd5e94389
Duane Billue <dbillue@gmail.com>
2020-07-31 09:18:09 -0400

    Initial check in.

---
+++ AV/VS/FamilyDemoAPIv2/Models/GetBirthStateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyDemoAPIv2.Models
+{
+    public class GetBirthStateDTO
+    {
+        public int StateId { get; set; }
+        
+        public string State { get; set; }
+
+        public string Abbreviation { get; set; }
+    }
+}
