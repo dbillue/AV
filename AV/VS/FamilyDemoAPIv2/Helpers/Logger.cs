bbd84915d0d31d50ea016d9980fea47fa5ab110a
Duane Billue <dbillue@gmail.com>
2020-07-31 09:17:45 -0400

    Removed un-needed using statements.

--- AV/VS/FamilyDemoAPIv2/Helpers/Logger.cs
+++ AV/VS/FamilyDemoAPIv2/Helpers/Logger.cs
@@ -1,9 +1,6 @@
 ﻿using FamilyDemoAPIv2.Models;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FamilyDemoAPIv2.Helpers
 {
