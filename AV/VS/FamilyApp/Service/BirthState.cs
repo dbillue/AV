0f976f19a85fdf0ba05aec7a23a37c4d6ed9e451
Duane Billue <dbillue@gmail.com>
2020-08-03 12:37:55 -0400

    Initial check in

---
+++ AV/VS/FamilyApp/Service/BirthState.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using FamilyApp.Model;
+using FamilyApp.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyApp.Service
+{
+    public class BirthState : IBirthState
+    {
+        SeriLog_Logger seriLogger = new SeriLog_Logger();
+
+        public string GetBirthState(Person person)
+        {
+            string stateName = string.Empty;
+
+            try
+            {
+                foreach (var state in person.birthState)
+                {
+                    if (person.StateId == state.StateId)
+                    {
+                        stateName = state.State;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                seriLogger.WriteError(ex.Message);
+            }
+
+            return stateName;
+        }
+
+        public int GetBirthStateId(Person person, List<Model.BirthState> birthStateList)
+        {
+            int stateId = 1;
+
+            try
+            {
+                foreach (var state in birthStateList)
+                {
+                    if (person.state == state.State)
+                    {
+                        stateId = state.StateId;
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                seriLogger.WriteError(ex.Message);
+            }
+
+            return stateId;
+        }
+    }
+}
---
+++ AV/VS/FamilyApp/Service/IBirthState.cs
@@ -0,0 +1,15 @@
+﻿using FamilyApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FamilyApp.Service
+{
+    interface IBirthState
+    {
+        string GetBirthState(Person person);
+
+        int GetBirthStateId(Person person, List<Model.BirthState> birthStateList);
+    }
+}
