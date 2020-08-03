2fb6d706dd007e3cd928191616fc5138d301c6a6
Duane Billue <dbillue@gmail.com>
2020-08-03 16:34:00 -0400

    Initial check in

--- AV/VS/FamilyDemoAPIv2/Models/GetBirthStateDTO.cs
+++ AV/VS/FamilyDemoAPIv2/Models/GetBirthStateDTO.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FamilyDemoAPIv2.Models
+﻿namespace FamilyDemoAPIv2.Models
 {
     public class GetBirthStateDTO
     {
         public int StateId { get; set; }
-        
+
         public string State { get; set; }
 
         public string Abbreviation { get; set; }
---
+++ AV/VS/FamilyDemoAPIv2/Models/GetPetTypesDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FamilyDemoAPIv2.Models
+{
+    public class GetPetTypesDTO
+    {
+        public Int32 PetTypeId { get; set; }
+
+        public string Type { get; set; }
+    }
+}
