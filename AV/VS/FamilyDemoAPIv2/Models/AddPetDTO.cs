26d1f1a97b7a4f90c6f82bf30f9941eb1855354d
Duane Billue <dbillue@gmail.com>
2020-08-04 17:28:36 -0400

    Initial check in

---
+++ AV/VS/FamilyDemoAPIv2/Models/AddPetDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FamilyDemoAPIv2.Models
+{
+    public class AddPetDTO
+    {
+        public Guid PetId { get; set; }
+
+        public string Name { get; set; }
+
+        public string NickName { get; set; }
+
+        public int PetTypeId { get; set; }
+
+        public Guid PersonId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
