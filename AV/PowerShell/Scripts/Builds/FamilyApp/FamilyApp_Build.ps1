2a8f0593167ea3365503d78b220003997fd798d7
Duane Billue <dbillue@gmail.com>
2020-08-09 20:47:44 -0400

    Initial check in

---
+++ AV/PowerShell/Scripts/Builds/FamilyApp/FamilyApp_Build.ps1
@@ -0,0 +1,21 @@
+﻿#Get-IISAppPool
+#Get-IISSite
+
+Clear
+
+iisreset /stop
+
+$blazorFamilyAppIISAppPool = Get-IISAppPool $IISsiteName
+$IISAppPool = "BlazorFamilyApp"
+$IISAppPoolState = Get-WebAppPoolState $IISAppPool
+$IISSiteName = "BlazorFamilyApp"
+$solutionPath = "C:\Users\dbill\source\repos\AV\AV\VS\FamilyApp\FamilyApp.sln"
+$outputPath = "C:\inetpub\wwwroot\BlazorFamilyApp\"
+
+Write-Host "Begin build and publish..."
+Write-Host "Solution name and directory:" $solutionPath
+Write-Host "Output directory:" $outputPath
+
+dotnet publish $solutionPath -o $outputPath
+
+iisreset /start
---
+++ AV/PowerShell/Scripts/Builds/FamilyApp/IIS_Sites_App_Pools.ps1
@@ -0,0 +1,38 @@
+﻿Clear
+
+$IISAppPoolState = Get-WebAppPoolState $IISAppPool
+Write-Host $IISAppPoolState.Value
+
+#--------------------------------#
+
+If($IISAppPoolState.Value -eq "Started")
+{
+    Write-Host "Stopping IIS app pool" $IISAppPool
+    Stop-WebAppPool -Name $IISAppPool -Passthru:$false
+
+    do
+    {
+        Write-Host "Stopping IIS app pool" $IISAppPool
+    }
+    while ($IISAppPoolState.Value -eq "Stopping")
+}
+
+Write-Host "Stopping IIS site" $IISsiteName
+Stop-IISSite -Name $IISSiteName -Confirm:$false
+
+#--------------------------------#
+
+If($IISAppPoolState.Value -eq "Stopped")
+{
+    Write-Host "Starting IIS app pool" $IISAppPool
+    Start-WebAppPool -Name $IISAppPool -Passthru:$false
+
+    do
+    {
+        Write-Host "Starting IIS app pool" $IISAppPool
+    }
+    while ($IISAppPoolState.Value -eq "Stopping")
+}
+
+Write-Host "Starting IIS site" $IISsiteName
+Start-IISSIte  -Name "BlazorFamilyApp"
