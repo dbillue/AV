ddf434f2fce1b84a0dd21440161da93193cb7d6e
Duane Billue <dbillue@gmail.com>
2020-08-10 11:32:38 -0400

    Added IISREST and removed app pool / website mods

--- AV/PowerShell/Scripts/Builds/FamilyApp/FamilyApp_Build.ps1
+++ AV/PowerShell/Scripts/Builds/FamilyApp/FamilyApp_Build.ps1
@@ -1,14 +1,18 @@
-﻿#Get-IISAppPool
+﻿# Author: Duane Billue
+# Date: 2020-08-10
+# Description: Automated publishing script for Blazor family application.
+
+#Get-IISAppPool
 #Get-IISSite
 
 Clear
 
 iisreset /stop
 
-$blazorFamilyAppIISAppPool = Get-IISAppPool $IISsiteName
 $IISAppPool = "BlazorFamilyApp"
-$IISAppPoolState = Get-WebAppPoolState $IISAppPool
 $IISSiteName = "BlazorFamilyApp"
+$blazorFamilyAppIISAppPool = Get-IISAppPool $IISsiteName
+$IISAppPoolState = Get-WebAppPoolState $IISAppPool
 $solutionPath = "C:\Users\dbill\source\repos\AV\AV\VS\FamilyApp\FamilyApp.sln"
 $outputPath = "C:\inetpub\wwwroot\BlazorFamilyApp\"
 
