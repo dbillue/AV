b72349a4e2eda40d54e0173478a0435c2dd89613
Duane Billue <dbillue@gmail.com>
2020-08-14 13:28:56 -0400

    Initial check in

---
+++ AV/PowerShell/Scripts/Azure/SQL_Server_Azure_Configure.ps1
@@ -0,0 +1,19 @@
+﻿Clear
+
+# Login to Azure
+# Connect-AzureRmAccount
+
+# Set database variables
+$resourceGroup = "AVSQLDemo"
+$server = Get-AzureRMSqlServer -ResourceGroupName $resourceGroup
+$logical_server = $server.ServerName
+$databaseName = Get-AzureRmSqlDatabase `
+                -ResourceGroupName $resourceGroup `
+                -ServerName $logical_server `
+                | Where DatabaseName -Like "Adventure*"
+$databaseName = $databaseName.DatabaseName
+
+# Set resrouce group and azure server
+az configure --defaults resource-group=$resourceGroup sql-server=$logical_server
+
+az configure --list-defaults
