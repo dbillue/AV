b8c1bb1c261c26dcba67e69ba2d172eec068388e
Duane Billue <dbillue@gmail.com>
2020-08-15 10:05:05 -0400

    Modified variable values

--- AV/PowerShell/Scripts/Azure/SQL_Server_database_Azure_Configure.ps1
+++ AV/PowerShell/Scripts/Azure/SQL_Server_database_Azure_Configure.ps1
@@ -1,33 +1,35 @@
 ﻿Clear
 
 # Login to Azure
-# Connect-AzureRmAccount
+#Connect-AzureRmAccount
 
 # Set database variables
-$resourceGroup = "AVSQLDemo"
+$resourceGroup = "AVSQLDemo2"
 $server = Get-AzureRMSqlServer -ResourceGroupName $resourceGroup
 $logical_server = $server.ServerName
 
 $database = Get-AzureRmSqlDatabase `
                 -ResourceGroupName $resourceGroup `
                 -ServerName $logical_server `
-                | Where DatabaseName -Like AdvWrks*
+                | Where DatabaseName -Like Demo*
 
 $databaseName = $database.DatabaseName
 
-Write-Host $databaseName
+#Write-Host $resourceGroup
+#Write-Host $logical_server
+#Write-Host $databaseName
 
 # Set resrouce group and azure server
-az configure --defaults resource-group=$resourceGroup sql-server=$logical_server
+az configure --defaults resource-group=$resourceGroup sql-server=$logical_server group=$resourceGroup
 
 # Obtain defaults
-# az configure --list-defaults
+#az configure --list-defaults
 
 # Obtain out database(s)
-# az sql db list
+#az sql db list
 
 # Obtain database properties
 az sql db show --name $databaseName
 
 # Obtain database usage and size
-az sql db list-usages --name $databaseName
+#az sql db list-usages --name $databaseName
