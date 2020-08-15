8ae4fa9ee67049f81125bc588a388ab843e2bb9e
Duane Billue <dbillue@gmail.com>
2020-08-15 10:18:55 -0400

    Initial check in

---
+++ AV/PowerShell/Scripts/Azure/SQL_Server_VM_Template.ps1
@@ -0,0 +1,18 @@
+﻿###########################################################
+# Author: Duane Billue
+# Date: 2020-08-15
+# Description: Azure SQL Server VM template
+###########################################################
+
+$adminSqlLogin = "av"
+$password = Read-Host "Your username is 'av'. Please enter a password for your Azure SQL Database server that meets the password requirements"
+# Prompt for local ip address
+$ipAddress = "73.184.17.17"
+# Get resource group and location and random string
+$resourceGroup = Get-AzResourceGroup | Where ResourceGroupName -like "Sandbox resource group name"
+$resourceGroupName = "Sandbox resource group name"
+$uniqueID = Get-Random -Minimum 100000 -Maximum 1000000
+$storageAccountName = "mslearnsa"+$uniqueID
+$location = $resourceGroup.Location
+# The logical server name has to be unique in the system
+$serverName = "aw-server$($uniqueID)"
