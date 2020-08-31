959140b94299341704a221ff7e394e1cb0720631
Duane Billue <dbillue@gmail.com>
2020-08-31 10:10:46 -0400

    Misc changes and added new SQL statements

--- AV/SQL Scripts/SQLServer/AV.API/AVI_API_Developer_Scripts.sql
+++ AV/SQL Scripts/SQLServer/AV.API/AVI_API_Developer_Scripts.sql
@@ -24,7 +24,7 @@ SELECT * FROM Persons ORDER BY CreateDate
 
 SELECT * FROM Persons
 WHERE 1 = 1
-	AND PersonId IN ('19C7017C-EE2D-411D-A72C-D8222DE47790')
+	AND PersonId IN ('04B15665-9C76-4392-94A3-04BA3101E0CF')
 	--AND LastName <> 'Billue'
 	--AND CONVERT(Date, CreateDate, 101) >= '2020-08-24'
 ORDER BY CreateDate
@@ -36,11 +36,13 @@ SELECT  COUNT(PersonID) RecCnt,
 			DATEDIFF(n, MIN(CreateDate), MAX(CreateDate)) 'Minutes'
 FROM Persons
 WHERE 1 = 1
-	AND FirstName = 'April'
+	--AND FirstName = 'April'
 
 SELECT person.*, bs.[State], pet.*
 FROM persons person LEFT OUTER JOIN pets pet ON person.PersonId = pet.PersonId
 	INNER JOIN birthstate bs ON bs.StateId = person.StateId
+WHERE 1 = 1
+		AND person.PersonId IN ('15E6D638-768F-4609-9CB4-49D49B24F9C4')
 ORDER BY person.LastName, person.FirstName, pet.Name
 
 INSERT INTO Persons (FirstName, MIddleName, LastName, Gender, Age, Country, City, StateId, DateOfBirth)
@@ -66,12 +68,18 @@ sp_helpindex Pets
 
 SELECT * FROM Pets ORDER BY Name
 
+SELECT * FROM Pets 
+WHERE 1 = 1
+	--AND PersonId = '5C641909-C2D9-4226-9F5F-9DC4AFF2C43E'
+	AND PetId IN ('B20F715F-E5B2-4D9F-9844-EFC7353AA131', 'DE0802EF-7326-41A7-AB62-00DE5873041A', '5c641909-c2d9-4226-9f5f-9dc4aff2c43e')
+ORDER BY CreateDate
+
 SELECT pet.*, pettype.[Type]
 FROM pets pet INNER JOIN PetTypes pettype ON pet.PetTypeId = pettype.PetTypeId
 ORDER BY pettype.Type, pet.Name
 
 INSERT INTO Pets (Name, NickName, PetTypeId, PersonId)
-VALUES ('MountainBelly', 'BellyBaby', 2, 'A3442212-DA30-4ECF-A730-005014A63C9F')
+VALUES ('MountainBelly', 'BellyBaby', 2, 'FFE36941-51F5-4FC9-9FEC-F5429AD61E11')
 
 -- DELETE FROM Pets WHERE PetId IN ('4F433572-A144-4E05-64D0-08D82A51E544')
 
