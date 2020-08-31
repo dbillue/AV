2e1b2d553bac41098ce9cdf0b0f51d61a5abf77f
Duane Billue <dbillue@gmail.com>
2020-08-31 10:33:38 -0400

    Initial check in

---
+++ AV/VS/FamilyAPITestHarness/Interfaces/IBirthStateController.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace FamilyAPITestHarness.Interfaces
+{
+    public interface IBirthStateController
+    {
+        Task TestAction();
+    }
+}
---
+++ AV/VS/FamilyAPITestHarness/Interfaces/IBirthStateService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace FamilyAPITestHarness.Interfaces
+{
+    public interface IBirthStateService
+    {
+        Task<string> GetStates(string route);
+    }
+}
