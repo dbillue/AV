edd2458e3b529800a2e2495a3e4369fa56c3a2f6
Duane Billue <dbillue@gmail.com>
2020-09-09 15:58:22 -0400

    Misc SQL mods

--- AV/SQL Scripts/SQLServer/AV.API/AVI_API_Developer_Scripts.sql
+++ AV/SQL Scripts/SQLServer/AV.API/AVI_API_Developer_Scripts.sql
@@ -24,7 +24,7 @@ SELECT * FROM Persons ORDER BY CreateDate
 
 SELECT * FROM Persons
 WHERE 1 = 1
-	AND PersonId IN ('04B15665-9C76-4392-94A3-04BA3101E0CF')
+	AND PersonId IN ('9ca42f1d-9673-47cb-a6c9-f2c98fd91adc')
 	--AND LastName <> 'Billue'
 	--AND CONVERT(Date, CreateDate, 101) >= '2020-08-24'
 ORDER BY CreateDate
@@ -66,12 +66,12 @@ Pets
 sp_help Pets
 sp_helpindex Pets
 
-SELECT * FROM Pets ORDER BY Name
+SELECT * FROM Pets ORDER BY PersonId, Name
 
 SELECT * FROM Pets 
 WHERE 1 = 1
 	--AND PersonId = '5C641909-C2D9-4226-9F5F-9DC4AFF2C43E'
-	AND PetId IN ('B20F715F-E5B2-4D9F-9844-EFC7353AA131', 'DE0802EF-7326-41A7-AB62-00DE5873041A', '5c641909-c2d9-4226-9f5f-9dc4aff2c43e')
+	AND PetId IN ('C9148BF4-4775-424E-AE16-B3C8F4674B3B')
 ORDER BY CreateDate
 
 SELECT pet.*, pettype.[Type]
